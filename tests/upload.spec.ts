9e2925edf5fceaaf1de71888fa973949a26599d8
Dilpreet Johal <dilpreetjohal@outlook.com>
2022-07-10 20:42:51 -0400

    wait commands

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -7,7 +7,7 @@ test.describe('Upload File', () => {
     await page.goto("https://practice.automationbro.com/cart/");
 
     // provide test file path
-    const filePath = path.join(__dirname, '../data/logotitle.png');
+    const filePath = path.join(__dirname, '../data/3mb-file.pdf');
     
     // upload test file
     await page.setInputFiles('input#upfile_1', filePath);
@@ -15,9 +15,16 @@ test.describe('Upload File', () => {
     // click the submit button
     await page.locator('#upload_1').click();
 
+    // hardcoded sleep - WRONG WAY
+    // await page.waitForTimeout(5000);
+    
+    // wait for condition
+    // await page.locator('#wfu_messageblock_header_1_1')
+    //   .waitFor({ state: 'visible', timeout: 10000 })
+
     // assertion
     await expect(page.locator('#wfu_messageblock_header_1_1'))
-      .toContainText('uploaded successfully');
+      .toContainText('uploaded successfully', {timeout: 10000});
   })
 
   test('should upload a test file on a hidden input field', async ({ page }) => {
