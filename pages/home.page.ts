a9f9be363cd7a4c0826aa3580caecb94ef3f6352
Dilpreet Johal <automationbro@gmail.com>
2022-07-18 07:35:21 -0400

    added homepage locators

--- pages/home.page.ts
+++ pages/home.page.ts
@@ -1,7 +1,18 @@
 class HomePage {
   page: any;
+  getStartedBtn: any;
+  headingText: any;
+  homeLink: any;
+  searchIcon: any;
+  navLinks: any;
+  
   constructor(page) {
     this.page = page;
+    this.getStartedBtn = page.locator('#get-started')
+    this.headingText = page.locator('text=Think different. Make different.')
+    this.homeLink = page.locator('#primary-menu:has-text("Home")')
+    this.searchIcon = page.locator('//*[@id="primary-menu"]//*[@class="tg-icon tg-icon-search"]')
+    this.navLinks = page.locator('#primary-menu li[id*=menu]')
   }
 }
 
--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -23,24 +23,28 @@ test.describe('Home', () => {
   })
 
   test('Click get started button using CSS Selector', async ({ page }) => {
+    homePage = new HomePage(page);
+
     // open url
     await page.goto('https://practice.automationbro.com');
     
     await expect(page).not.toHaveURL(/.*#get-started/);
 
     // click the button
-    await page.locator('#get-started').click();
+    await homePage.getStartedBtn.click()
 
     // verify url has #get-started
     await expect(page).toHaveURL(/.*#get-started/);
   })
 
   test('Verify heading text is visible using text selector', async ({ page }) => {
+    homePage = new HomePage(page);
+    
     // open url
     await page.goto('https://practice.automationbro.com');
 
     // find the text locator
-    const headingText = page.locator('text=Think different. Make different.')
+    const headingText = await homePage.headingText
 
     // verify heading text is visible
     await expect(headingText).not.toBeHidden();
@@ -48,29 +52,34 @@ test.describe('Home', () => {
   })
 
   test('Verify home link is enabled using text and css selector', async ({ page }) => {
+    homePage = new HomePage(page);
+
     // open url
     await page.goto('https://practice.automationbro.com');
 
     // find the home text
-    // const homeText = await page.locator('#primary-menu >> text=Home')
-    const homeText = page.locator('#primary-menu:has-text("Home")')
+    const homeText = await homePage.homeLink
 
     // verify home text is enabled
     await expect(homeText).toBeEnabled();
   })
 
   test('Verify search icon is visible using xpath selector', async ({ page }) => {
+    homePage = new HomePage(page);
+
     // open url
     await page.goto('https://practice.automationbro.com');
 
     // find the search icon
-    const searchIcon = page.locator('//*[@id="primary-menu"]//*[@class="tg-icon tg-icon-search"]')
+    const searchIcon = await homePage.searchIcon
 
     // verify search icon is visible
     await expect(searchIcon).toBeVisible();
   })
 
   test('Verify text of all nav links', async ({ page }) => {
+    homePage = new HomePage(page);
+
     const expectedLinks = [
       "Home",
       "About",
@@ -84,7 +93,7 @@ test.describe('Home', () => {
     await page.goto('https://practice.automationbro.com');
 
     // find the nav links
-    const navLinks = page.locator('#primary-menu li[id*=menu]')
+    const navLinks = await homePage.navLinks
 
     // print out all the links
     for (const el of await navLinks.elementHandles()) {
