341b9965ee5a7f61e96cbe2d052b6543bb3bdc29
Dilpreet Johal <automationbro@gmail.com>
2022-07-26 18:30:59 -0400

    upload component implementation

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -1,8 +1,13 @@
 import { test, expect } from '@playwright/test';
+import CartPage from '../pages/cart.page';
 const path = require('path');
 
 test.describe('Upload File', () => {
+  let cartPage: CartPage;
+
   test('should upload a test file', async ({ page }) => {
+    cartPage = new CartPage(page);
+
     // Open url
     await page.goto("https://practice.automationbro.com/cart/");
 
@@ -10,20 +15,10 @@ test.describe('Upload File', () => {
     const filePath = path.join(__dirname, '../data/3mb-file.pdf');
     
     // upload test file
-    await page.setInputFiles('input#upfile_1', filePath);
-
-    // click the submit button
-    await page.locator('#upload_1').click();
-
-    // hardcoded sleep - WRONG WAY
-    // await page.waitForTimeout(5000);
-    
-    // wait for condition
-    // await page.locator('#wfu_messageblock_header_1_1')
-    //   .waitFor({ state: 'visible', timeout: 10000 })
+    cartPage.uploadComponent().uploadFile(filePath);
 
     // assertion
-    await expect(page.locator('#wfu_messageblock_header_1_1'))
+    await expect(cartPage.uploadComponent().successTxt)
       .toContainText('uploaded successfully', {timeout: 10000});
   })
 
