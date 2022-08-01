629ad87763974d746d9efc7df022ddcc040ed1d3
Dilpreet Johal <automationbro@gmail.com>
2022-08-01 09:23:31 -0400

    signed-in state tests update

--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,26 +1,14 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test.describe.serial('My Account', () => {
-  let page: Page
-
-  test.beforeAll(async ({browser}) => {
-    page = await browser.newPage()
+test.describe('My Account', () => {
+  test('Access Orders', async ({ page }) => {
     await page.goto('/my-account')
-    await page.locator('#username').fill('practiceuser1')
-    await page.locator('#password').fill('PracticePass1!')
-    await page.locator('[value="Log in"]').click()
-    await expect(page.locator('a:has-text("Logout")'))
-      .toBeVisible()
-  })
-
-  test('Access Orders', async () => {
-    // await page.goto('/my-account')
     await page.locator(`li a[href*='orders']`).click()
     await expect(page).toHaveURL(/.*orders/)
   });
 
-  test('Access Downloads', async () => {
-    // await page.goto('/my-account')
+  test('Access Downloads', async ({ page }) => {
+    await page.goto('/my-account')
     await page.locator(`li a[href*='downloads']`).click()
     await expect(page).toHaveURL(/.*downloads/)
   });
